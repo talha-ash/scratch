ebda8535c476baad00e879aa823f218c6ac88e9a
xoronedge <ta.xit@yandex.com>
2022-04-30 19:38:55 +0500

    minor tweaks

--- lib/scratch_web/api/resolvers/recipe_resolver.ex
+++ lib/scratch_web/api/resolvers/recipe_resolver.ex
@@ -7,8 +7,8 @@ defmodule ScratchWeb.Resolvers.Recipe do
     with {:ok, %Recipes.Recipe{} = recipe} <- Recipes.create_recipe(args) do
       {:ok, recipe}
     else
-      {:error, message} ->
-        {:error, message}
+      {:error, changeset} ->
+        {:error, changeset}
     end
   end
 
@@ -17,8 +17,8 @@ defmodule ScratchWeb.Resolvers.Recipe do
            Recipes.update_recipe(args) do
       {:ok, recipe}
     else
-      {:error, message} ->
-        {:error, message}
+      {:error, changeset} ->
+        {:error, changeset}
     end
   end
 
@@ -27,8 +27,8 @@ defmodule ScratchWeb.Resolvers.Recipe do
            Recipes.update_recipe_ingredients(args.id, Map.delete(args, :id)) do
       {:ok, %{ingredients: recipe.ingredients}}
     else
-      {:error, message} ->
-        {:error, message}
+      {:error, changeset} ->
+        {:error, changeset}
     end
   end
 
@@ -37,8 +37,8 @@ defmodule ScratchWeb.Resolvers.Recipe do
            Recipes.update_recipe_cooking_steps(args.id, Map.delete(args, :id)) do
       {:ok, %{cooking_steps: recipe.cooking_steps}}
     else
-      {:error, message} ->
-        {:error, message}
+      {:error, changeset} ->
+        {:error, changeset}
     end
   end
 
@@ -47,8 +47,8 @@ defmodule ScratchWeb.Resolvers.Recipe do
            Recipes.update_recipe_images(args.id, Map.delete(args, :id)) do
       {:ok, %{recipe_images: recipe.recipe_images}}
     else
-      {:error, message} ->
-        {:error, message}
+      {:error, changeset} ->
+        {:error, changeset}
     end
   end
 
@@ -57,5 +57,40 @@ defmodule ScratchWeb.Resolvers.Recipe do
 
     recipe = Recipes.get_recipe(args.id)
     {:ok, recipe}
+  end
+
+  def like_recipe(_parent, args, %{context: %{current_user: current_user}}) do
+    args = Map.put(args, :user_id, current_user.id)
+
+    with nil <- Recipes.get_like_recipe(args),
+         {:ok, %Recipes.Like{} = recipe} <-
+           Recipes.like_recipe(args) do
+      {:ok, %{message: "like successfully"}}
+    else
+      %Recipes.Like{} = recipe_like ->
+        Recipes.unlike_recipe(recipe_like)
+        {:ok, %{message: "unlike successfully"}}
+
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  def create_category(_parent, args, %{context: %{current_user: current_user}}) do
+    args = Map.put(args, :user_id, current_user.id)
+
+    with {:ok, %Recipes.Category{} = category} <-
+           Recipes.create_category(args) do
+      {:ok, category}
+    else
+      {:error, changeset} ->
+        {:error, changeset}
+    end
+  end
+
+  def get_categories(_parent, args, %{context: %{current_user: current_user}}) do
+    args = Map.put(args, :user_id, current_user.id)
+
+    {:ok, Recipes.get_categories(current_user.id)}
   end
 end
